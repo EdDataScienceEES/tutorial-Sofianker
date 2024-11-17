4fdc0554eaf148a414dcd981be2750c08187b23e
Sofianker <ankersofiamay@gmail.com>
2024-11-17 10:08:02 +0000

    adding more maps

--- R-script.R
+++ R-script.R
@@ -13,24 +13,25 @@ library(patchwork)
 library(ggdensity)
 #Load Data
 #Can be found in repository
+register_stadiamaps("d3dae1ed-f4db-43ac-b64b-c15410e3ac30",write = FALSE)
 North_Sea<-read_csv("~/Desktop/Ecological Measurement /download_3400_'ME5301_ActualPositions_RC20130128.zip'.csv")
 str(North_Sea)
 
 #Mapping----
 #Define bbox
 bbox<- c(-12.650070,49.968448,5.279617,59.880324)
-map<-get_stadiamap(bbox = bbox, zoom=5, maptype = "stamen_terrain")
+get_stadiamap(bbox = bbox, zoom=5, maptype = "stamen_terrain")
 ggmap(map)
 #create map using geompoint and ggmap
 get_stadiamap(bbox, zoom = 5, maptype = "stamen_terrain") %>% ggmap() +
-  geom_point(aes(x = DecLon84, y = DecLat84), data = North_Sea, colour = "blue", size = 2)
+  geom_point(aes(x = DecLon84, y = DecLat84, colour = CruiseID), data = North_Sea, size = 2)
 
 #creat map using qmplot
 qmplot(DecLon84, DecLat84, data = North_Sea, maptype = "stamen_toner_lite", color = I("blue"))
 
 qmplot(DecLon84, DecLat84, data = North_Sea, maptype = "stamen_terrain", color = I("blue"))
 
-qmplot(DecLon84, DecLat84, data = North_Sea, maptype = "stamen_watercolor", color = I("blue"))
+qmplot(DecLon84, DecLat84, data = North_Sea, maptype = "stamen_terrain", color = CruiseID)
 
 #map the density of sampling locations 
 (hdr_map <- ggmap(map) + 
